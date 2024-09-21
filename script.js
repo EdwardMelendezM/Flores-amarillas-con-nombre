ecca90c8ef4284e03dce64082e8eede8c372d296
EdwardMelendezM <edward.melendez.mendigure@gmail.com>
2024-09-21 10:04:08 -0500

    add changes

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 onload = () => {
+  document.documentElement.requestFullscreen().catch(err => {
+    console.log("Error al entrar en pantalla completa: ", err);
+  });
   document.body.classList.remove("container");
+  const nameOfPerson = document.getElementById("name-person");
+  // Get name of the queryparams
+    const urlParams = new URLSearchParams(window.location.search);
+    nameOfPerson.innerHTML = urlParams.get("name") ?? '';
+    //Que la clase sobreponga encima de cualquier cosas
+  // nameOfPerson.classList.add("container");
 };
